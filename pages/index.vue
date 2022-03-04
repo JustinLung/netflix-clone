3e616978bbf64d71144b6cf0d1a6a6154793f96a
JustinLung <justinlung77@gmail.com>
2022-03-04 19:49:37 +0100

    🍿 Latest movie section made

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,29 @@
   <div>
     <section>
       <img
-        :src="'https://image.tmdb.org/t/p/original' + movies[3].poster_path"
+        :src="'https://image.tmdb.org/t/p/original' + movies[0].poster_path"
         alt="Movie Poster"
         class="movie-banner"
       />
-      <h1>🎥 {{ movies[3].original_title }}</h1>
-      <p class="synopsis">{{ movies[3].overview }}</p>
+      <h1>🎥 {{ movies[0].original_title }}</h1>
+      <p class="synopsis">{{ movies[0].overview }}</p>
       <div class="button-container">
         <button class="cta-white">🍿 Add to List</button>
         <button class="cta-transparent">📖 More information</button>
       </div>
+      <div class="gradient"></div>
     </section>
     <section>
       <h2>🎉 Latest</h2>
+      <div id="movie-grid" class="movie-grid">
+        <div class="movie" v-for="(movie, index) in movies" :key="index">
+          <img
+            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+            alt="Movie Poster"
+            class="movie-poster"
+          />
+        </div>
+      </div>
     </section>
     <section>
       <h2>🍿 Recommended</h2>
@@ -47,13 +57,30 @@ export default {
 
 <style scoped>
 section:first-child {
-  height: 40em;
+  height: 100vh;
   padding: 0 2.5em;
   display: flex;
   flex-direction: column;
   justify-content: center;
   object-fit: cover;
   position: relative;
+}
+
+.gradient {
+  position: absolute;
+  bottom: -1%;
+  left: 0;
+  height: 0;
+  padding-bottom: calc(33.5% - 92px + 30px);
+  width: 100%;
+  background-image: linear-gradient(
+    transparent,
+    rgba(25, 26, 26, 0.4) 10%,
+    rgba(25, 26, 26, 0.6) 20%,
+    rgba(25, 26, 26, 0.8) 60%,
+    var(--dark-grey)
+  );
+  z-index: -1;
 }
 
 section:not(first-child) {
@@ -75,14 +102,16 @@ h1,
 p {
   padding: 0;
   margin: 0;
+  text-shadow: 2px 2px 4px rgb(0 0 0 / 45%);
 }
 
 h1 {
   font-size: 3rem;
+  max-width: 20em;
 }
 
 .synopsis {
-  font-size: 1.5rem;
+  font-size: 1.4vw;
   width: 100%;
   max-width: 30em;
   padding-top: 0.5rem;
@@ -107,6 +136,17 @@ button {
   opacity: 0.8;
 }
 
+.movie-grid {
+  display: flex;
+  gap: 1em;
+  overflow-x: auto; 
+}
+
+.movie-poster {
+  max-width: 15em;
+  border-radius: 0.5em;
+}
+
 .cta-transparent {
   border-radius: 0.5em;
   color: var(--white);
@@ -125,6 +165,10 @@ button {
   h1 {
     margin-top: 4em;
     font-size: 2.5rem;
+  }
+
+  h2 {
+    font-size: 1.3rem;
   }
 
   .synopsis {
