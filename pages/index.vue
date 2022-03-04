4e715ab77fc160f6e2e2f74b0217690c9f548a11
JustinLung <justinlung77@gmail.com>
2022-03-04 12:07:15 +0100

    api has been fetched.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <section>
-      <h1>Movie Title</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-        ducimus ratione alias beatae vel deleniti commodi architecto
-        consectetur, rerum minus.
-      </p>
+      <img
+        :src="'https://image.tmdb.org/t/p/original' + movies[0].poster_path"
+        alt="Movie Poster"
+        class="movie-banner"
+      />
+      <h1>{{ movies[0].original_title }}</h1>
+      <p>{{ movies[0].overview }}</p>
       <div class="button-container">
         <button class="cta-white">Add to List</button>
         <button class="cta-transparent">More information</button>
@@ -25,8 +26,12 @@ export default {
   async fetch() {
     this.movies = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?api_key=5b75818e63dfdb396cadedf77425b334&language=en-US&page=1"
-    ).then((res) => res.json());
-    console.log(this.movies)
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        return data.results;
+      });
+    console.log(this.movies);
   },
 };
 </script>
@@ -38,6 +43,19 @@ section {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  object-fit: cover;
+  position: relative;
+}
+
+.movie-banner {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  opacity: .8;
 }
 
 h1,
